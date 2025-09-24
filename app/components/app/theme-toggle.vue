22552f75cfcffab6e570fd057c10e0b5691076a2
AlexCourtney18 <lex.courtney18@gmail.com>
2025-09-24 13:44:17 -0500

    add light/dark mode toggle

---
+++ app/components/app/theme-toggle.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate mx-4">
+    <input v-model="isDark" type="checkbox">
+    <Icon class="swap-on" name="tabler:sun" size="1.5rem" />
+    <Icon class="swap-off" name="tabler:moon-filled" size="1.5rem" />
+  </label>
+</template>
